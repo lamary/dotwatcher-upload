58a7ab281e6d4363ce63d39492448ab6c4bd25c0
Jon Heslop <jon@jonheslop.com>
2019-11-15 16:53:59 +0000

    Fix apostrophes

--- pages/api/create-results.js
+++ pages/api/create-results.js
@@ -11,14 +11,14 @@ export default async function handle(req, res) {
     let i = 2 // to offset zero index plus the fact the csv header isnt counted
     for (const row of resultsData) {
       const rowOriginal = {...row}
-      const { rows: riderResults } = await db.query(`SELECT id, riders.name FROM riders WHERE riders.name = '${row.name}'`) || []
+      const { rows: riderResults } = await db.query(`SELECT id, riders.name FROM riders WHERE riders.name = '${row.name.replace(/'/g, '’')}'`) || []
 
       if (riderResults.length) {
         // console.log('rider found', riderResults[0].name, riderResults[0].id)
         riderID = riderResults[0].id
       } else {
         // console.log('rider not found')
-        const newRider = await db.query(`INSERT INTO riders(name) VALUES($1) RETURNING id`, [row.name])
+        const newRider = await db.query(`INSERT INTO riders(name) VALUES($1) RETURNING id`, [row.name.replace(/'/g, '’')])
         riderID = newRider.rows[0].id
         // console.log('New rider created:', newRider.rows[0].id)
       }
