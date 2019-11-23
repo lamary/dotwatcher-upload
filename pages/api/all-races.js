a262c2220fb708b34d445a5559151c4f53a0a935
Jon Heslop <jon@jonheslop.com>
2019-11-23 08:10:28 +0000

    Order races alphabetically

--- pages/api/all-races.js
+++ pages/api/all-races.js
@@ -3,7 +3,7 @@ import db from '../../database'
 export default async function handle(req, res) {
   try {
     // const { rows: races } = await client.query(`SELECT name, year, length, id, slug FROM races;`)
-    const { rows: races } = await db.query(`SELECT name, array_agg(year) as years, array_agg(length) as lengths, array_agg(id) as ids, array_agg(slug) as slugs FROM races GROUP BY name;`)
+    const { rows: races } = await db.query(`SELECT name, array_agg(year) as years, array_agg(length) as lengths, array_agg(id) as ids, array_agg(slug) as slugs FROM races GROUP BY name ORDER BY name;`)
 
     const formattedRaces = races.map(race => {
       return {
