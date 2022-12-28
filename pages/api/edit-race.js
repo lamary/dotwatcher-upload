70457c0fac7a2a3da19686f18ef94a039cb7840c
Alex Windett <windettalex@gmail.com>
2022-12-28 15:17:22 +0000

    remove string interpoation from edit results query

--- pages/api/edit-race.js
+++ pages/api/edit-race.js
@@ -1,28 +1,37 @@
-import db from "../../database";
+import db from '../../database';
 
 export default async function handle(req, res) {
-  const formData = req.body;
+	const formData = req.body;
 
-  const { slug } = formData;
+	const { slug } = formData;
 
-  const values = {
-    name: formData.name,
-    startLocation: formData["start-location"],
-    endLocation: formData["end-location"],
-    length: formData.length,
-    terrain: formData.terrain,
-    description: encodeURIComponent(formData.description),
-  };
+	const values = {
+		name: formData.name,
+		startLocation: formData['start-location'],
+		endLocation: formData['end-location'],
+		length: formData.length,
+		terrain: formData.terrain,
+		description: encodeURIComponent(formData.description),
+	};
 
-  const query = `UPDATE races set name = '${values.name}', startlocation = '${values.startLocation}', finishlocation = '${values.endLocation}', length = ${values.length}, terrain = '${values.terrain}', description = '${values.description}' where races.slug = '${slug}'`;
+	const query = `UPDATE races set name = $1, startlocation = $2, finishlocation = $3, length = $4, terrain = $5, description = $6 where races.slug = $7`;
 
-  try {
-    const result = await db.query(query);
-    return res.json({ status: 200, race: result.rows[0] });
-  } catch (error) {
-    console.log(error);
-    return res.json({ status: 500, title: "something was bad with your inputs", error });
-  }
+	try {
+		const result = await db.query(query, [
+			values.name,
+			values.startLocation,
+			values.endLocation,
+			values.length,
+			values.terrain,
+			values.description,
+			slug,
+		]);
+
+		return res.json({ status: 200, race: result.rows[0] });
+	} catch (error) {
+		console.log(error);
+		return res.json({ status: 500, title: 'something was bad with your inputs', error });
+	}
 }
 
 // CREATE TABLE races(
